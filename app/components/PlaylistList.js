b837c7558a56f4b1559f00e91467ef88346cf260
janvi01 <janvibajo1@gmail.com>
2024-02-03 12:53:21 +0530

    feat: display playlist details, thumbnail and title

--- app/components/PlaylistList.js
+++ app/components/PlaylistList.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const PlaylistList = ({ playlists, onDeletePlaylist }) => {
-  const handleDeletePlaylist = async (playlistId) => {
-    try {
-      await onDeletePlaylist(playlistId);
-    } catch (error) {
-      console.error("Error deleting playlist:", error);
-    }
+  const [playlistDetails, setPlaylistDetails] = useState({});
+  const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
+  useEffect(() => {
+    const fetchPlaylistDetails = async (url) => {
+      try {
+        // Extract playlist ID from YouTube URL
+        const playlistId = getPlaylistIdFromUrl(url);
+
+        // Make a request to the YouTube Data API to get details
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/playlists?part=snippet&key=${apiKey}&maxResults=1&id=${playlistId}`
+        );
+        console.log(apiKey);
+
+        if (response.ok) {
+          const data = await response.json();
+
+          if (data.items.length > 0) {
+            const playlistDetails = data.items[0].snippet;
+            setPlaylistDetails((prevDetails) => ({
+              ...prevDetails,
+              [url]: {
+                title: playlistDetails.title,
+                thumbnailUrl: playlistDetails.thumbnails.default.url,
+              },
+            }));
+          } else {
+            setPlaylistDetails((prevDetails) => ({
+              ...prevDetails,
+              [url]: null,
+            }));
+          }
+        } else {
+          console.error(
+            "Error fetching playlist details:",
+            response.statusText
+          );
+          setPlaylistDetails((prevDetails) => ({
+            ...prevDetails,
+            [url]: null,
+          }));
+        }
+      } catch (error) {
+        console.error("Error fetching playlist details:", error);
+        setPlaylistDetails((prevDetails) => ({
+          ...prevDetails,
+          [url]: null,
+        }));
+      }
+    };
+
+    playlists.forEach((playlist) => {
+      if (!playlistDetails[playlist.url]) {
+        fetchPlaylistDetails(playlist.url);
+      }
+    });
+  }, [playlists]);
+
+  const getPlaylistIdFromUrl = (url) => {
+    // Extract playlist ID from YouTube URL
+    const urlParams = new URLSearchParams(new URL(url).search);
+    return urlParams.get("list");
   };
 
   return (
@@ -17,9 +74,24 @@ const PlaylistList = ({ playlists, onDeletePlaylist }) => {
           key={playlist.id}
           className="flex justify-between items-center border-b py-2"
         >
-          <p className="text-blue-500">{playlist.url}</p>
+          {playlistDetails[playlist.url] ? (
+            <>
+              <div className="flex items-center">
+                <img
+                  src={playlistDetails[playlist.url].thumbnailUrl}
+                  alt="Playlist Thumbnail"
+                  className="w-12 h-12 mr-2 rounded-md"
+                />
+                <p className="text-blue-500">
+                  {playlistDetails[playlist.url].title}
+                </p>
+              </div>
+            </>
+          ) : (
+            <p className="text-blue-500">{playlist.url}</p>
+          )}
           <button
-            onClick={() => handleDeletePlaylist(playlist.id)}
+            onClick={() => onDeletePlaylist(playlist.id)}
             className="text-red-500 hover:text-red-700 focus:outline-none focus:text-red-700"
           >
             Delete
