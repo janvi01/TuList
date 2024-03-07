4e10da35dda8e66a886e40dd06f60d75eac97b6b
Janvi Bajoria <janvibajo1@gmail.com>
2024-03-07 16:56:26 +0530

    fix: website url preview

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,20 @@ export default function RootLayout({ children }) {
           content="A Youtube Playlist Manager to keep all your YouTube playlists at one place. Get started now."
           key="desc"
         />
-        <meta property="og:title" content="TuList 🚀" />
         <meta property="og:description" content="A Youtube Playlist Manager." />
-        <meta property="og:image" content="/app/assets/preview.png" />
+        <meta property="og:image" content="/preview.png" />
         <meta property="og:url" content="https://tulist-web.vercel.app/" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="TuList 🚀" />
+        <meta
+          name="twitter:description"
+          content="A Youtube Playlist Manager to keep all your YouTube playlists at one place. Get started now."
+        />
+        <meta
+          name="twitter:image"
+          content="https://tulist-web.vercel.app/preview.png"
+        />
+        <meta name="twitter:site" content="@janvibajo01" />
       </head>
       <body className="bg-gradient-to-r from-slate-900 to-stone-800">
         <AuthContextProvider>
