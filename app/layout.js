1447728c14c4f24de58e83209dedd713e69bec02
Janvi Bajoria <janvibajo1@gmail.com>
2024-03-07 16:07:54 +0530

    added page preview image

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>TuList ✨</title>
+        <title>TuList 🚀</title>
         <meta
           name="description"
           content="A Youtube Playlist Manager to keep all your YouTube playlists at one place. Get started now."
           key="desc"
         />
+        <meta property="og:title" content="TuList 🚀" />
+        <meta property="og:description" content="A Youtube Playlist Manager." />
+        <meta property="og:image" content="/app/assets/preview.png" />
+        <meta property="og:url" content="https://tulist-web.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
       </head>
       <body className="bg-gradient-to-r from-slate-900 to-stone-800">
         <AuthContextProvider>
