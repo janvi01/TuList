ce974b36c79ec3acf5b8153df2f95271938de8de
Janvi Bajoria <janvibajo1@gmail.com>
2024-03-12 21:11:58 +0530

    refactor hero and features component

--- app/components/HeroLayout/Features.js
+++ app/components/HeroLayout/Features.js
@@ -3,6 +3,7 @@ import {
   CloudArrowUpIcon,
   CheckIcon,
 } from "@heroicons/react/24/outline";
+import { useMemo } from "react";
 
 const featuresList = [
   {
@@ -30,40 +31,52 @@ const featuresList = [
   },
 ];
 
+function FeatureItem({ name, description, Icon }) {
+  return (
+    <div className="relative pl-16">
+      <dt className="text-base font-semibold leading-7 text-indigo-600">
+        <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+          <Icon className="h-6 w-6 text-white" aria-hidden="true" />
+        </div>
+        {name}
+      </dt>
+      <dd className="mt-2 text-base leading-7 text-neutral-300">
+        {description}
+      </dd>
+    </div>
+  );
+}
+
 export default function Features() {
+  const FeatureComponents = useMemo(() => {
+    return featuresList.map((feature) => (
+      <FeatureItem
+        key={feature.name}
+        name={feature.name}
+        description={feature.description}
+        Icon={feature.icon}
+      />
+    ));
+  }, []);
+
   return (
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
-            Track efforlessly
+            Track effortlessly
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             What TuList can do?
           </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 text-gray-400">
             Are you learning or following any course or watching any web series
-            on Youtube ? Add and watch all your Youtube Playlist at one place.
+            on Youtube? Add and watch all your Youtube Playlist at one place.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {featuresList.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-indigo-600">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-neutral-300">
-                  {feature.description}
-                </dd>
-              </div>
-            ))}
+            {FeatureComponents}
           </dl>
         </div>
       </div>
--- app/components/HeroLayout/HeroSection.js
+++ app/components/HeroLayout/HeroSection.js
@@ -6,59 +6,59 @@ import Demo from "../../assets/demo.png";
 function HeroSection() {
   const { handleSignIn } = UserAuth();
   return (
-    <div className="text-white">
-      <div className="max-w mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
-        <div className="mt-5 max-w-3xl text-center mx-auto">
-          <h1 className="block font-bold text-4xl md:text-5xl lg:text-6xl">
-            YouTube Playlist{" "}
-            <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
-              Manager
-            </span>
-          </h1>
-        </div>
+    <div className="text-white max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+      <div className="text-center max-w-3xl mx-auto">
+        <h1 className="font-bold text-4xl md:text-5xl lg:text-6xl">
+          YouTube Playlist{" "}
+          <span className="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">
+            Manager
+          </span>
+        </h1>
+      </div>
 
-        <div className="mt-5 max-w-3xl text-center mx-auto">
-          <p className="text-lg">
-            Want to follow a YouTube Playlist? Worry not!
-            <br /> TuList is an intuitive platform built to track and organize
-            your <br />
-            YouTube playlist videos in a better way.
-          </p>
-        </div>
+      <div className="mt-5 text-center max-w-3xl mx-auto">
+        <p className="text-lg">
+          Want to follow a YouTube Playlist? Worry not!
+          <br /> TuList is an intuitive platform built to track and organize
+          your <br />
+          YouTube playlist videos in a better way.
+        </p>
+      </div>
 
-        <div className="mt-8 gap-3 flex justify-center">
-          <button
-            className="inline-flex justify-center cursor-pointer items-center gap-x-3 bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4"
-            onClick={handleSignIn}
+      <div className="mt-8 flex justify-center">
+        <button
+          className="inline-flex justify-center items-center gap-x-3 bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4"
+          onClick={handleSignIn}
+        >
+          Get started
+          <svg
+            className="w-4 h-4 flex-shrink-0"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
           >
-            Get started
-            <svg
-              className="flex-shrink-0 w-4 h-4"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
-          </button>
+            <path
+              d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4 mt-8">
+        <div className="flex flex-col items-center space-y-2 px-3 py-2 rounded-full bg-gray-800 hover:border-gray-600">
+          <p>More features incoming soon ✨ Stay tuned 😉</p>
         </div>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <div className="flex flex-col items-center mt-8 space-y-2 px-3 py-2 rounded-full bg-gray-800 hover:border-gray-600">
-            <p>More features incoming soon ✨ Stay tuned 😉</p>
-          </div>
+        <div className="mt-8">
           <Image
             src={Demo}
             width={900}
-            height={"auto"}
+            height={600}
             alt="TuList Demo"
             priority={true}
-            className="mt-8 object-cover shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] rounded-3xl"
+            className="object-cover shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] rounded-3xl"
           />
         </div>
       </div>
